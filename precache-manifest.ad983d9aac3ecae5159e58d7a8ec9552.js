42f3c5eb66521f41afe838e10882e0e8cb43bb8f
root <saood011@yahoo.com>
2019-11-04 16:02:46 +0100

    Updates

---
+++ precache-manifest.ad983d9aac3ecae5159e58d7a8ec9552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058d860fb5fab978d34af113a13e0cf1",
+    "url": "/RandomUserWithHooks/index.html"
+  },
+  {
+    "revision": "0e16348631649412b6cf",
+    "url": "/RandomUserWithHooks/static/css/main.7c3ae2d3.chunk.css"
+  },
+  {
+    "revision": "475f8ef5af317805a990",
+    "url": "/RandomUserWithHooks/static/js/2.91546406.chunk.js"
+  },
+  {
+    "revision": "0e16348631649412b6cf",
+    "url": "/RandomUserWithHooks/static/js/main.4a809e35.chunk.js"
+  },
+  {
+    "revision": "4c79573439b41bde54fd",
+    "url": "/RandomUserWithHooks/static/js/runtime-main.1fe5aec9.js"
+  }
+]);
